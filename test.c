14402bd2fa1d563b7a29442f0ef127cd46088cc8
Jeong-Rae <kkwjdfo@gmail.com>
2023-03-07 22:02:03 +0900

    first

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    for (int i = 0; i < 100; i++){
+        a = i;
+        printf("%d\n", a);
+    }
+    printf("Hello, world!\n");
+    return 0;
+}
